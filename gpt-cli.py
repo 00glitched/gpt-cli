35481a46287ce6fa6cf266b82844f38bf26a180b
00glitched <conaeze3@gmail.com>
2025-03-16 22:32:51 -0300

    Update: Pretty-printing

--- gpt-cli.py
+++ gpt-cli.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
+parsing=True
 import os
-import argparse
-from g4f.client import Client
+try:
+    import argparse
+except ImportError:
+    print("Módulo no instalado: pip install argparse")
+    parsing=False
+try:
+    from g4f.client import Client
+except ImportError:
+    print("Módulo no instalado: pip install g4f")
+try:
+    import mdv
+except ImportError:
+    print("Módulo no instalado: pip install mdv")
 
 HOME=str(os.getenv("HOME"))
 
-def chat_with_gpt(prompt, model="gpt-4o-mini"):
+def chat_with_gpt(prompt, model="gpt-4o-mini", pretty=False):
     api_key = os.getenv("OPENAI_API_KEY")
     if not api_key:
             print("Error: Debes configurar la variable de entorno OPENAI_API_KEY.")
@@ -17,7 +29,16 @@ def chat_with_gpt(prompt, model="gpt-4o-mini"):
             messages=[{"role": "user", "content": prompt}],
             web_search=False
         )
-        print(response.choices[0].message.content)
+        msg=""
+        if pretty == True:
+            msg=formatted = mdv.main(str(response.choices[0].message.content),"AXC_THEME")
+            print(msg)
+        elif pretty == False:
+            msg=response.choices[0].message.content
+            print(msg)
+        else:
+            print("Error: '"+pretty+"' no es un argumento valido.")
+        
     finally:
         pass
 
@@ -25,8 +46,10 @@ def main():
     parser = argparse.ArgumentParser(description="CLI para consultar ChatGPT")
     parser.add_argument("prompt", type=str, help="Pregunta o mensaje para ChatGPT")
     parser.add_argument("--model", type=str, default="gpt-4o-mini", help="Modelo de OpenAI a usar (por defecto: gpt-4o-mini)")
+    parser.add_argument("-p","--pretty", action="store_true", help="Impresion bonita en pantalla estilo markdown")
+
     args = parser.parse_args()
-    chat_with_gpt(args.prompt, args.model)
+    chat_with_gpt(args.prompt, args.model, args.pretty)
 
 if __name__ == "__main__":
     main()
